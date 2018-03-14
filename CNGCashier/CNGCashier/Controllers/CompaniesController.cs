fd6f0b70be34d6bee9d8197691af30ddbf365739
jeremybinuya <jeremybinuya@24c4c3b8-7fc9-11e6-b3b5-ff44f5e38317>
2018-03-14 02:25:37 +0000

    CNG Cashiering Vehicle and Company

---
+++ CNGCashier/CNGCashier/Controllers/CompaniesController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CNGCashier.Models;
+using PagedList;
+
+namespace CNGCashier.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        private CNGCashierDBContext db = new CNGCashierDBContext();
+
+        // GET: Companies
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.AddressSortParm = String.IsNullOrEmpty(sortOrder) ? "address_desc" : "";
+            ViewBag.ContactPSortParm = String.IsNullOrEmpty(sortOrder) ? "contactp_desc" : "";
+            ViewBag.ContactNSortParm = String.IsNullOrEmpty(sortOrder) ? "contactn_desc" : "";
+            ViewBag.ActiveSortParm = String.IsNullOrEmpty(sortOrder) ? "active_desc" : "";
+            ViewBag.PrefixSortParm = String.IsNullOrEmpty(sortOrder) ? "prefix_desc" : "";
+            var companies = from c in db.Companies select c;
+
+            //Paging
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
+
+            //Search
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                companies = companies.Where(c => c.Name.Contains(searchString));
+            }
+
+            //Sorting the table for companies
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    companies = companies.OrderByDescending(c => c.Name);
+                    break;
+                case "address_desc":
+                    companies = companies.OrderByDescending(c => c.Address);
+                    break;
+                case "contactp_desc":
+                    companies = companies.OrderByDescending(c => c.ContactPerson);
+                    break;
+                case "contactn_desc":
+                    companies = companies.OrderByDescending(c => c.ContactNo);
+                    break;
+                case "active_desc":
+                    companies = companies.OrderByDescending(c => c.Active);
+                    break;
+                case "prefix_desc":
+                    companies = companies.OrderByDescending(c => c.Prefix);
+                    break;
+                default:
+                    companies = companies.OrderBy(c => c.Name);
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(companies.ToPagedList(pageNumber, pageSize));
+            //return View(db.Companies.ToList());
+        }
+
+        // GET: Companies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Companies/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Address,ContactPerson,ContactNo,Active,Prefix")] Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Companies.Add(company);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(company);
+        }
+
+        // GET: Companies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Company company = db.Companies.Find(id);
+            if (company == null)
+            {
+                return HttpNotFound();
+            }
+            return View(company);
+        }
+
+        // POST: Companies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Address,ContactPerson,ContactNo,Active,Prefix")] Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(company).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(company);
+        }
+
+        // GET: Companies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Company company = db.Companies.Find(id);
+            if (company == null)
+            {
+                return HttpNotFound();
+            }
+            return View(company);
+        }
+
+        // POST: Companies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Company company = db.Companies.Find(id);
+            db.Companies.Remove(company);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CNGCashier/CNGCashier/Controllers/VehiclesController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CNGCashier.Models;
+using PagedList;
+
+namespace CNGCashier.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        private CNGCashierDBContext db = new CNGCashierDBContext();
+
+        // GET: Vehicles
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.MakeSortParm = String.IsNullOrEmpty(sortOrder) ? "make_desc" : "";
+            ViewBag.YearSortParm = String.IsNullOrEmpty(sortOrder) ? "year_desc" : "";
+            ViewBag.ModelSortParm = String.IsNullOrEmpty(sortOrder) ? "model_desc" : "";
+            ViewBag.CnNoSortParm = String.IsNullOrEmpty(sortOrder) ? "cnno_desc" : "";
+            ViewBag.LicenseNoSortParm = String.IsNullOrEmpty(sortOrder) ? "licenseNo_desc" : "";
+            ViewBag.EngineNoSortParm = String.IsNullOrEmpty(sortOrder) ? "engineno_desc" : "";
+            ViewBag.ChasisNoSortParm = String.IsNullOrEmpty(sortOrder) ? "chasisno_desc" : "";
+            ViewBag.ColorSortParm = String.IsNullOrEmpty(sortOrder) ? "color_desc" : "";
+            ViewBag.ActiveSortParm = String.IsNullOrEmpty(sortOrder) ? "active_desc" : "";
+            var vehicles = from v in db.Vehicles select v;
+
+            //Paging
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
+
+            //Search
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                vehicles = vehicles.Where(c => c.Make.Contains(searchString));
+            }
+
+            //Sorting the table for companies
+            switch (sortOrder)
+            {
+                case "make_desc":
+                    vehicles = vehicles.OrderByDescending(v => v.Make);
+                    break;
+                case "year_desc":
+                    vehicles = vehicles.OrderByDescending(v => v.Year);
+                    break;
+                case "model_desc":
+                    vehicles = vehicles.OrderByDescending(v => v.Model);
+                    break;
+                case "cnno_desc":
+                    vehicles = vehicles.OrderByDescending(v => v.CnNo);
+                    break;
+                case "licenseNo_desc":
+                    vehicles = vehicles.OrderByDescending(v => v.LicenseNo);
+                    break;
+                case "engineno_desc":
+                    vehicles = vehicles.OrderByDescending(v => v.EngineNo);
+                    break;
+                case "chasisno_desc":
+                    vehicles = vehicles.OrderByDescending(v => v.ChasisNo);
+                    break;
+                case "color_desc":
+                    vehicles = vehicles.OrderByDescending(v => v.Color);
+                    break;
+                case "active_desc":
+                    vehicles = vehicles.OrderByDescending(v => v.Active);
+                    break;
+                default:
+                    vehicles = vehicles.OrderBy(v => v.Make);
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(vehicles.ToPagedList(pageNumber, pageSize));
+            //return View(db.Vehicles.ToList());
+        }
+
+
+        // GET: Vehicles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Vehicles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Make,Year,Model,CnNo,LicenseNo,EngineNo,ChasisNo,Color,Active")] Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Vehicles.Add(vehicle);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(vehicle);
+        }
+
+        // GET: Vehicles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vehicle vehicle = db.Vehicles.Find(id);
+            if (vehicle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicle);
+        }
+
+        // POST: Vehicles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Make,Year,Model,CnNo,LicenseNo,EngineNo,ChasisNo,Color,Active")] Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vehicle).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(vehicle);
+        }
+
+        // GET: Vehicles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vehicle vehicle = db.Vehicles.Find(id);
+            if (vehicle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicle);
+        }
+
+        // POST: Vehicles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Vehicle vehicle = db.Vehicles.Find(id);
+            db.Vehicles.Remove(vehicle);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
